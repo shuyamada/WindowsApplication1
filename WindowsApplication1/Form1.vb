6457a1d33245ad2975435e293a5925511b030aba
shuyamada <fatringo@gmail.com>
2014-06-26 15:13:49 +0900

    1st test

---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,10 @@
+﻿Namespace Hello World{
+    Class Prgram{
+        static void Main(string[]args){
+            Console.WriteLine("Hello, world!");
+            Console.ReadKey();
+            }
+    }
+    End Class
+}
+End Namespace
